371649f1483419ebf1ec2a6e58cf29559bb87c0a
chenxiang01 <chenxiang01@rong360.com>
2023-07-13 17:23:22 +0800

    blog-react页面跳转

--- src/constant/ajax_config.js
+++ src/constant/ajax_config.js
@@ -3,7 +3,7 @@ const host = "http://120.46.41.223:8080/data";
 
 import {useNavigate} from 'react-router-dom';
 
-export const login = (data) => {
+export const login = (data, success_call_back, error_call_back) => {
     fetch(host + "/login", {
         method: 'POST',
         headers: {
@@ -12,19 +12,9 @@ export const login = (data) => {
         body: JSON.stringify(data)
     }).then(res => {
             if (res.status >= 400 && res.status < 500) {
-                throw new Error('用户名或者密码错误');
+                throw  new Error('用户名或者密码错误');
             }
             return res.json();
         }
-    ).then(
-        (result) => {
-            console.log("login_result", result);
-            const history = useNavigate();
-            history.push('/');
-        },
-        (error) => {
-            console.log("error-res:");
-            console.log(error);
-        }
-    );
+    ).then(success_call_back, error_call_back);
 };
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,87 +2,92 @@ import {Button, Checkbox, Form, Input} from 'antd';
 import Layout from "antd/es/layout";
 import ArrowRightOutlined from "@ant-design/icons/lib/icons/ArrowRightOutlined";
 import {login} from "../constant/ajax_config";
+import {useNavigate} from 'react-router-dom';
 
 const {Header, Content, Footer, Sider} = Layout;
 
-const onFinish = (values) => {
-    login(values);
-    console.log('Success:', values);
-};
+
 const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
 };
 
-const LoginForm = () => (
-    <Form
-        name="basic"
-        labelCol={{
-            span: 8,
-        }}
-        wrapperCol={{
-            span: 16,
-        }}
-        style={{
-            maxWidth: 3500,
-        }}
-        initialValues={{
-            remember: true,
-        }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-    >
-        <Form.Item
-            label="用户名"
-            name="username"
-            rules={[
-                {
-                    required: true,
-                    message: '输入用户名',
-                },
-            ]}
-        >
-            <Input/>
-        </Form.Item>
-
-        <Form.Item
-            label="密码"
-            name="password"
-            rules={[
-                {
-                    required: true,
-                    message: '输入密码！',
-                },
-            ]}
-        >
-            <Input.Password/>
-        </Form.Item>
-
-        <Form.Item
-            name="remember"
-            valuePropName="checked"
+const LoginForm = () => {
+    const history = useNavigate();
+    const onFinish = (values) => {
+    login(values, ()=>history.push('/'),
+        (error)=>{});
+};
+    return (
+        <Form
+            name="basic"
+            labelCol={{
+                span: 8,
+            }}
             wrapperCol={{
-                offset: 8,
                 span: 16,
             }}
-        >
-            <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item
-            wrapperCol={{
-                offset: 8,
-                span: 16,
+            style={{
+                maxWidth: 3500,
             }}
+            initialValues={{
+                remember: true,
+            }}
+            onFinish={(values) => onFinish(values)}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
         >
+            <Form.Item
+                label="用户名"
+                name="username"
+                rules={[
+                    {
+                        required: true,
+                        message: '输入用户名',
+                    },
+                ]}
+            >
+                <Input/>
+            </Form.Item>
 
-            <Button htmlType="submit" style={{color: "#af50b6"}}>
-                <ArrowRightOutlined/>
-                进入心情空间
-            </Button>
-        </Form.Item>
-    </Form>
-);
+            <Form.Item
+                label="密码"
+                name="password"
+                rules={[
+                    {
+                        required: true,
+                        message: '输入密码！',
+                    },
+                ]}
+            >
+                <Input.Password/>
+            </Form.Item>
+
+            <Form.Item
+                name="remember"
+                valuePropName="checked"
+                wrapperCol={{
+                    offset: 8,
+                    span: 16,
+                }}
+            >
+                <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item
+                wrapperCol={{
+                    offset: 8,
+                    span: 16,
+                }}
+            >
+
+                <Button htmlType="submit" style={{color: "#af50b6"}}>
+                    <ArrowRightOutlined/>
+                    进入心情空间
+                </Button>
+            </Form.Item>
+        </Form>
+    );
+}
 
 const LoginPage = () => (
 

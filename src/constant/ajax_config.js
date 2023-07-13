8c1f60ac7ddd20858ad51c638670057dbe146931
chenxiang01 <chenxiang01@rong360.com>
2023-07-13 16:26:19 +0800

    blog-react页面跳转

--- src/constant/ajax_config.js
+++ src/constant/ajax_config.js
@@ -1,25 +1,22 @@
 const host = "http://120.46.41.223:8080/data";
 
 
-
-import { useHistory } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 
 export const login = (username, password) => {
-    const params = new URLSearchParams();
-    fetch(host + "/login?" + params.toString(), {
+    fetch(host + "/login", {
         method: 'POST',
-        data: JSON.stringify({username: 'value', password: password})
+        body: JSON.stringify({username: username, password: password})
     }).then(res => {
-            console.log("res", res);
-            const result = res.json();
-            console.log(res, result);
-            return result;
+        return res.json();
         }
     ).then(
         (result) => {
-            const history = useHistory();
-            console.log(result);
-            history.push('/blog');
+            console.log("login_result" + result);
+            if (result.status === 200) {
+                const history = useNavigate();
+                history.push('/');
+            }
 
         },
         (error) => {

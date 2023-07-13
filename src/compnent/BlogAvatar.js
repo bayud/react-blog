a6d818ba70cb054e38b58b6e25013274c1f31899
chenxiang01 <chenxiang01@rong360.com>
2023-07-14 02:45:22 +0800

    blog-react 头像

--- src/compnent/BlogAvatar.js
+++ src/compnent/BlogAvatar.js
@@ -2,11 +2,14 @@ import {UserOutlined} from '@ant-design/icons';
 import {Avatar} from 'antd';
 import {get_file_url} from "../constant/ajax_config";
 
-const BlogAvatar = (image_url) => (
-    <Avatar style={{backgroundColor: '#87d068'}} icon={<UserOutlined/>}
-            size={80}
-            src={get_file_url(image_url)}/>
+const BlogAvatar = (image_url) => {
+    console.log(image_url);
+    return (
+        <Avatar style={{backgroundColor: '#87d068'}} icon={<UserOutlined/>}
+                size={80}
+                src={get_file_url(image_url)}/>
 
-    // <Avatar size="large" icon={<UserOutlined />} />
-);
+        // <Avatar size="large" icon={<UserOutlined />} />
+    )
+};
 export default BlogAvatar;

3c05d8f04d35680e00bff47731296eb1a0cd40dc
chenxiang01 <chenxiang01@rong360.com>
2023-07-14 02:58:08 +0800

    blog-react file_name

--- src/compnent/BlogAvatar.js
+++ src/compnent/BlogAvatar.js
@@ -3,13 +3,10 @@ import {Avatar} from 'antd';
 import {get_file_url} from "../constant/ajax_config";
 
 const BlogAvatar = (props) => {
-    console.log(props);
     return (
         <Avatar style={{backgroundColor: '#87d068'}} icon={<UserOutlined/>}
                 size={80}
                 src={get_file_url(props.image_url)}/>
-
-        // <Avatar size="large" icon={<UserOutlined />} />
     )
 };
 export default BlogAvatar;

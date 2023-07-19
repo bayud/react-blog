21d713f4cb9530a579905ab2f301d9397ba1770f
chenxiang2 <chenxiang2@myhexin.com>
2023-07-19 13:14:17 +0800

    confirm

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import BlogPages from "./pages/BlogPages";
 import EditPage from "./pages/EditPage";
-import {BrowserRouter, HashRouter, Navigate, Route, Routes, Link, Switch, Redirect} from "react-router-dom";
+import { BrowserRouter, HashRouter, Navigate, Route, Routes, Link, Switch, Redirect } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import BottomNotice from "./compnent/notice/BottomNotice";
 import ConfirmModal from "./compnent/notice/ConfirmDialog";
-import {observer} from "mobx-react";
+import { observer } from "mobx-react";
 import App from "antd/es/app";
 
 
 const myApp = () => (<App>
     <BrowserRouter basename="/view">
-        <title>心情空间</title>
         <Routes>
-            <Route path="/" element={<Navigate to="/blog"/>}/>
-            <Route path="/login" element={<LoginPage/>}/>
-            <Route path="/blog" element={<BlogPages/>}/>
-            <Route path="/edit" element={<EditPage/>}/>
-            <Route path="*" element={<Navigate to="/"/>}/>
+            <Route path="/" element={<Navigate to="/blog" />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/blog" element={<BlogPages />} />
+            <Route path="/edit" element={<EditPage />} />
+            <Route path="*" element={<Navigate to="/" />} />
         </Routes>
-        <BottomNotice/>
-    </BrowserRouter>
-    <ConfirmModal/>
+        </BrowserRouter>
+        <BottomNotice />
+    <ConfirmModal />
 </App>);
 
 

e545a5270e8ab17e1b1dfd1323a6c7267e3f473f
chenxiang2 <chenxiang2@myhexin.com>
2023-07-19 13:28:09 +0800

    load

--- src/pages/BlogPages.js
+++ src/pages/BlogPages.js
@@ -38,7 +38,21 @@ const BlogPages = () => {
     const [done, setDone] = useState(false);
     loadingRef.current = loading;
     const navigate = useNavigate();
-
+    const load = (page) => {
+        console.log("load...page:", page);
+        const data = ([...Array(10).keys()]).map((item, i) => ({
+            name: "cx",
+            date: "07月06日18:00",
+            content: "第" + (i + (page - 1) * 10) + "條內容"
+        }));
+        return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve(data);
+                }, 1000)
+            }
+        )
+    };
+    
     // 监听页面滚动
     const handleOnScroll = () => {
         console.log("页面发生滚动...");
